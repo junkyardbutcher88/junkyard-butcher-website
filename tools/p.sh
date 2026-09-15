2f7ed06f2c205c6cd4eeed637a8fe0a8d5b207e8
Gabe <gabequair@gmail.com>
2026-09-15 00:17:35 -0700

    Stills colour on hover; reels stay mono until played

--- tools/p.sh
+++ tools/p.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")/.."
 git add -A
-git commit -m "DECISIONS: record mono-until-engaged as a site-wide rule"
+git commit -m "Stills colour on hover; reels stay mono until played"
 git push
 echo
 git log --oneline -2
